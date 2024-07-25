91175aeca4afd9d19e36e95dc8ca3755dc6f1a36
m1kp <yarikasik@inbox.ru>
2024-07-25 13:46:43 +0300

    Update m1kpeeeeee.lua

--- m1kpeeeeee.lua
+++ m1kpeeeeee.lua
@@ -1,5 +1,6 @@
+--не рассказывай пж никому что это разводняк подыграй плз просто мунсек щяс не работает потом зашифрую
 if game.PlaceId == 6961824067 then
 
-game.Players.LocalPlayer:Kick("Fling things and people thinks you most likey exploited (hacked) while playing last time. You can play again in about 60 minute(s). See you soon!")
+game.Players.LocalPlayer:Kick("Fling things and people thinks you most likey exploited (hacked) while playing last time. You can play again in about 24 hour(s). See you soon!")
 
 end
